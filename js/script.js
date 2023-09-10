f83c11c3cd8f17f2a04b3df81312268a82118658
momona-watanabe <140260256+momona-watanabe@users.noreply.github.com>
2023-09-10 22:55:41 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
+//ハンバーガーメニュー
+$(".openbtn").click(function(){
+  $(this).toggleClass('active');
+  $(".menu").toggleClass('active');
+  $(".cart_icon_w").toggleClass('active');
+  $(".header_ttl").toggleClass('active');
+  return false;
+});
+
+
+
+
+//Menu hover_line
+$('.menu_list a').hover (
+  function(){
+    $(this).find('.hover_line').addClass('active')
+  },
+  function(){
+    $(this).find('.hover_line').removeClass('active');
+  },
+);
+
+
+$('.link_more').hover (
+  function(){
+    $(this).find('span').addClass('active')
+  },
+  function(){
+    $(this).find('span').removeClass('active');
+  },
+);
+
+
 
 
 
 //loadingアニメーション
-//フェードアウトした後に動かしたいJSをまとめる
 $(window).on('load',function(){
+
+//フェードアウトした後に動かしたいJSをまとめる
   $("#loading").delay(1500).fadeOut('slow',function(){
     $('body').addClass('appear'); //フェードアウト後bodyにappearクラス付与
   });
